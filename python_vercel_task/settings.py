63e762fdf36fd4f321060c06997d8590b1130423
hector_alfonso <hectorbog30@gmail.com>
2024-11-19 22:13:21 -0300

    a

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['https://task-python-4jgfavemk-hector-alfonsos-projects.vercel.app/']
 
 
 # Application definition
